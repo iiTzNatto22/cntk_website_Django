e8a65ba92139758032ff949333916acb1e9dafd7
iiTzNatto22 <113847252+iiTzNatto22@users.noreply.github.com>
2023-05-08 22:26:10 +0200

    ne

--- model/cntknnModel.py
+++ model/cntknnModel.py
@@ -13,3 +13,22 @@ num_samples = 60000
 batch_size = 64 
 learning_rate = 0.1 
 
+class Batch_Reader(object):
+    def __init__(self, data, label):
+        self.data = data 
+        self.label = label
+        self.num_sample = data.shape[0]
+
+    def next_batch(self, batch_size):
+        index = random.sample(range(self.num_sample), batch_size)
+        return self.data[index,:].astype(float),self.label[index,:].astype(float)
+    
+
+mnist = fetch_openml('mnist_784')
+
+train_data = mnist.data[:num_samples,:]
+train_label = mnist.target[:num_samples]
+test_data = mnist.data[num_samples:,:]
+test_label = mnist.target[num_samples:]
+
+enc = OneHotEncoder()
